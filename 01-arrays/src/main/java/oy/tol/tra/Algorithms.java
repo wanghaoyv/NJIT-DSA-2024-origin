005dd287adec3caf681f88063e269da208e2b9e3
wanghaoyv <2860988570@qq.com>
2024-04-02 14:01:40 +0800

    wwww

--- 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
+++ 01-arrays/src/main/java/oy/tol/tra/Algorithms.java
@@ -1,39 +1,29 @@
 package oy.tol.tra;
 
 public class Algorithms {
+    public static <T> void swap(T[] array, int index1, int index2) {
+        T tmp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = tmp;
+    }
+
     public static <T extends Comparable<T>> void sort(T[] array) {
-        if (array == null || array.length <= 1) {
-            return;
-        }
-
-        for (int i = 1; i < array.length; i++) {
-            T current = array[i];
-            int j = i - 1;
-
-            while (j >= 0 && array[j].compareTo(current) > 0) {
-                array[j + 1] = array[j];
-                j--;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j].compareTo(array[j + 1]) > 0) {
+                    swap(array, j, j + 1);
+                }
             }
-
-            array[j + 1] = current;
         }
     }
 
     public static <T> void reverse(T[] array) {
-        if (array == null || array.length < 2) {
-            return;
-        }
-
         int left = 0;
         int right = array.length - 1;
-
         while (left < right) {
-            T temp = array[left];
-            array[left] = array[right];
-            array[right] = temp;
+            swap(array, left, right);
             left++;
             right--;
         }
     }
-
 }
--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -1,18 +1,9 @@
 package oy.tol.tra;
 
-/**
- * A simple array of student grades to be used in testing
- * misbehaving algorithm for reversing the array.
- */
 public class Grades {
 
    private Integer[] grades = null;
 
-   /**
-    * A constructor for building IntArrays.
-    * 
-    * @param grades the plain Java integer array with numbers to add.
-    */
    public Grades(Integer[] grades) {
       this.grades = new Integer[grades.length];
       for (int counter = 0; counter < grades.length; counter++) {
@@ -21,41 +12,13 @@ public class Grades {
    }
 
    public void reverse() {
-
-      for (int i = 0; i < grades.length / 2; i++) {
-         int temp = grades[i];
-         grades[i] = grades[grades.length - i - 1];
-         grades[grades.length - i - 1] = temp;
-      }
+      Algorithms.reverse(grades);
    }
 
-   /**
-    * Sorts the array to ascending order.
-    */
    public void sort() {
-
-      boolean sorted = false;
-      int n = grades.length;
-      while (!sorted) {
-         sorted = true;
-         for (int i = 0; i < n - 1; i++) {
-            if (grades[i] > grades[i + 1]) {
-               int tmp = grades[i];
-               grades[i] = grades[i + 1];
-               grades[i + 1] = tmp;
-               sorted = false;
-            }
-         }
-         n--;
-      }
-
+      Algorithms.sort(grades);
    }
 
-   /**
-    * Returns the plain Java int [] array for investigation.
-    * 
-    * @return The int array.
-    */
    public Integer[] getArray() {
       return grades;
    }
