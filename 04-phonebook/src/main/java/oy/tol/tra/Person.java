4c363729164e25b26f917e26f11f56f864d3f330
wanghaoyv <126221103+wanghaoyv@users.noreply.github.com>
2024-04-09 21:39:26 +0800

    Update Person.java

--- 04-phonebook/src/main/java/oy/tol/tra/Person.java
+++ 04-phonebook/src/main/java/oy/tol/tra/Person.java
@@ -31,16 +31,24 @@ public class Person implements Comparable<Person> {
         return getFullName();
     }
 
-    /**
-     * first and last name of the person.
-     * 
-     * @return Hash value of the person.
-     */
+
     @Override
     public int hashCode() {
-        return getFullName().hashCode()%100000000;
+       
+        return customStringHash(getFullName());
     }
-
+    public static int customStringHash(String str) {
+        int hash = 0;
+        int prime = 31; 
+    
+        
+        for (int i = 0; i < str.length(); i++) {
+          
+            hash = hash * prime + str.charAt(i);
+        }
+    
+        return hash;
+    }
     @Override
     public boolean equals(Object other) {
         if (other instanceof Person) {
@@ -50,19 +58,7 @@ public class Person implements Comparable<Person> {
     }
    
 
-    /**
-     * Compares two persons, this and the other one.
-     * <p>
-     * In a phonebook, persons are identified by the last and first names.
-     * So if a person is the same or another, depends on if they have
-     * the same name. You can use String.compareTo in implementing this.
-     * <p>
-     * Return <0 if the person's full name (as string) is smaller than the other's.
-     * Return 0 if the full name is identical.
-     * Return >0 if the other persons full name is larger (as string).
-     * Note: String class also implements <code>compareTo()</code> you can use here.
-     * @returns Returns 0 if persons are the same otherwise depending on the full name, <0 or >0.
-     */
+
     @Override
     public int compareTo(Person other) {
         return getFullName().compareTo(other.getFullName());
